582a616c55ae5ccf4f545d33e01222a9856f474c
Vashist <kaushik.hurnaum@gmail.com>
2017-11-20 20:41:57 +0400

    displaying board

--- Bataille_Naval OOP.py
+++ Bataille_Naval OOP.py
@@ -2,12 +2,23 @@ class Board:
     def __init__(self,n):
         #empty array
         self.__grid = [[0 for x in range(0,n)] for y in range(0,n)]
-        print(self.__grid)
+        self.display()
 
     def modifyGrid(self,value,x,y):
         self.__grid[x][y] = value
 
+    def display(self):
+        for line in self.__grid:
+            for column in line:
+                if column == 0:
+                    print(" . ",end=' ')
+                if column == 1:
+                    print(" x ",end=' ')
+                if column == 2:
+                    print(" o ",end=' ')
+            print()
+
+
+
+
 board1 = Board(10)
-
-
-
