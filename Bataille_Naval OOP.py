e4636d62afd48afe1f7c180b62fe522ae677115b
Vashist <kaushik.hurnaum@gmail.com>
2017-11-22 17:30:59 +0400

    classes completed

--- Bataille_Naval OOP.py
+++ Bataille_Naval OOP.py
@@ -20,6 +20,57 @@ class Board:
 
 
 
+class Ship:
+    def __setLife(self,value):
+        self.__life = value
+
+    def setHits(self,value):
+        self.__hits = value
+
+    def __setName(self,value):
+        self.name = value
+
+    def __init__(self,name,size):
+        self.__setName(name)
+        self.setHits(0)
+        self.__setLife(size)
+
+    def printInfo(self):
+        print(self.name)
+        print(self.__life)
+        print(self.__hits)
+
+
+
+
+class Player:
+    def __setName(self,value):
+        self.__name = value
+
+    def setScore(self,value):
+        self.score = value
+
+    def setNumberOfShips(self,value):
+        self.NumberOfShips = value
+
+    def __init__(self):
+        self.ships = []
+        name = input("Please enter your name: ")
+        self.__setName(name)
+        self.setScore(0)
+        self.setNumberOfShips(5)
+        submarine = Ship("Submarine",5)
+        self.ships.append(submarine)
+        print(self.ships[0].name)
+        print(self.__name)
+        print(self.score)
+        print(self.NumberOfShips)
+
+
+player1 = Player()
+
+ship1 = Ship("Submarine", 5)
+ship1.printInfo()
 
 board1 = Board(10)
 print()
