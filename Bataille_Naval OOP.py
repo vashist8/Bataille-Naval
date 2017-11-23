6a154bbc7b1a7136133fa400f9bd4280dde29879
Vashist <kaushik.hurnaum@gmail.com>
2017-11-23 09:31:32 +0400

    defining methods

--- Bataille_Naval OOP.py
+++ Bataille_Naval OOP.py
@@ -30,10 +30,14 @@ class Ship:
     def __setName(self,value):
         self.name = value
 
-    def __init__(self,name,size):
+    def __setSign(self,value):
+        self.sign = value
+
+    def __init__(self,name,size,sign):
         self.__setName(name)
         self.setHits(0)
         self.__setLife(size)
+        self.__setSign(sign)
 
     def printInfo(self):
         print(self.name)
@@ -44,6 +48,10 @@ class Ship:
 
 
 class Player:
+
+    def __setID(self,value):
+        self._id = value
+
     def __setName(self,value):
         self.__name = value
 
@@ -53,26 +61,49 @@ class Player:
     def setNumberOfShips(self,value):
         self.NumberOfShips = value
 
-    def __init__(self):
-        self.ships = []
+    def __init__(self,pid):
         name = input("Please enter your name: ")
         self.__setName(name)
+        self.board = Board(10)
+        self.ships = []
+        self._id = pid
         self.setScore(0)
         self.setNumberOfShips(5)
-        submarine = Ship("Submarine",5)
-        self.ships.append(submarine)
-        print(self.ships[0].name)
-        print(self.__name)
-        print(self.score)
-        print(self.NumberOfShips)
+        submarine = Ship("Submarine",3)
+        carrier = Ship("Carrier",5)
+        battleship = Ship("Battleship",4)
+        cruiser = Ship("Cruiser",3)
+        destoyer = Ship("Destroyer",2)
+        self.ships = [submarine,carrier,battleship,cruiser,destoyer]
 
 
-player1 = Player()
 
-ship1 = Ship("Submarine", 5)
-ship1.printInfo()
+def isGridEmpty(board,x,y):
+    return board[x][y] == 0
 
-board1 = Board(10)
-print()
-board1.modifyGrid(1,5,5)
-board1.display()
+def promptOrientation(name):
+    print("In which orientation do you want to place your" + name + "?")
+    orientation = ""
+    while orientation != "h" or orientation != "v":
+        orientation = "Please enter h for horizontal orientation or v for vertical orientation"
+
+    return orientation
+
+def isShipAdjacent(orientation,board,x,y):
+    if orientation == "h":
+        return board[x+1][y] == 1 or board[x-1][y] == 1
+
+    if orientation == "v":
+        return  board[x][y+1] ==1 or board[x][y-1] == 1
+
+
+
+
+
+def isShipAdjacent(board,x,y):
+    return (board[x+1][y] == 1 or board[x-1][y] == 1
+
+def main():
+    player1 = Player(1)
+
+
